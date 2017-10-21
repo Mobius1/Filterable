82db1ee76dc4821dc54466a329e2692acfde7f99
Mobius1 <admin@twitchaddict.tv>
2017-10-21 21:14:39 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,63 @@
+# filterable
+[![npm version](https://badge.fury.io/js/vanilla-datatables-filterable.svg)](https://badge.fury.io/js/vanilla-datatables-filterable) [![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/Mobius1/filterable/blob/master/LICENSE) ![](http://img.badgesize.io/Mobius1/filterable/master/datatable.filterable.min.js) ![](http://img.badgesize.io/Mobius1/filterable/master/datatable.filterable.min.js?compression=gzip&label=gzipped)
+
+A Vanilla-DataTables extension to allow for exporting to various formats.
+
+NOTE: This extension is only compatable with `v2.0.0` and above of `Vanilla-DataTables`.
+
+[DEMO](https://s.codepen.io/Mobius1/debug/VMQEzw)
+
+---
+
+### Install
+
+## Bower
+```
+bower install vanilla-datatables-filterable --save
+```
+
+## npm
+```
+npm install vanilla-datatables-filterable --save
+```
+
+---
+
+### Browser
+
+Grab the files from one of the CDNs and include them in your page:
+
+```html
+<link href="https://unpkg.com/vanilla-datatables-filterable@latest/datatable.filterable.min.css" rel="stylesheet" type="text/css">
+<script src="https://unpkg.com/vanilla-datatables-filterable@latest/datatable.filterable.min.js" type="text/javascript"></script>
+
+//or
+
+<link href="https://cdn.jsdelivr.net/npm/vanilla-datatables-filterable@latest/datatable.filterable.min.css" rel="stylesheet" type="text/css">
+<script src="https://cdn.jsdelivr.net/npm/vanilla-datatables-filterable@latest/datatable.filterable.min.js" type="text/javascript"></script>
+```
+
+You can replace `latest` with the required release number.
+
+NOTE: Make sure the above js file is included AFTER the main Vanilla-DataTables js file.
+
+---
+
+## Options
+
+---
+
+## Public Methods
+
+
+---
+
+## Changelog
+
+`v0.0.1`
+* Initial commit.
+
+
+---
+
+Copyright © 2017 Karl Saunders | MIT license
---
+++ datatable.filterable.js
@@ -0,0 +1,150 @@
+/*! filterable 0.0.1
+ * © 2017 Karl Saunders
+ */
+/**
+ * @summary     filterable
+ * @description Vanilla-DataTables extension that adds inputs to each column for filtering
+ * @version     0.0.1
+ * @file        datatable.filterable.js
+ * @author      Karl Saunders
+ * @contact     mobius1@gmx.com
+ * @copyright   Copyright 2017 Karl Saunders
+ *
+ *
+ * This source file is free software, available under the following license:
+ *   MIT license - https://github.com/Mobius1/Vanilla-DataTables/blob/master/LICENSE
+ *
+ * This source file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
+ *
+ * For details please refer to: https://github.com/Mobius1/Vanilla-DataTables
+ */
+if (window.DataTable) {
+    DataTable.extend("filterable", function(instance, options, utils) {
+
+        /**
+         * Default config
+         * @type {Object}
+         */
+        var defaults = {
+            classes: {
+                filter: "dt-filter",
+                filterable: "dt-filterable",
+            }
+        };
+
+        /**
+         * Get the closest matching ancestor
+         * @param  {Object}   el         The starting node.
+         * @param  {Function} fn         Callback to find matching ancestor.
+         * @return {Object|Boolean}      Returns the matching ancestor or false in not found.
+         */
+        var closest = function(el, fn) {
+            return el && el !== document.body && (fn(el) ? el : closest(el.parentNode, fn));
+        };
+
+        /**
+         * Main lib
+         * @param {Object} config User config
+         */
+        var Filter = function(config) {
+            this.config = utils.extend(defaults, config);
+        }
+
+        /**
+         * Init instance
+         * @return {Void}
+         */
+        Filter.prototype.init = function() {
+
+            if (this.initialised) return;
+
+            var that = this,
+                o = that.config;
+
+            that.inputs = [];
+            that.row = utils.createElement("tr");
+
+            utils.each(instance.table.header.cells, function(cell) {
+                that.add(cell.index, o.placeholders && o.placeholders[cell.index] ? o.placeholders[cell.index] : "Search " + cell.content)
+            });
+
+            that.row.addEventListener("input", function(e) {
+                var input = closest(e.target, function(el) {
+                    return el.nodeName === "INPUT";
+                });
+
+                if (input) {
+                    instance.columns().search(input.parentNode.cellIndex, input.value);
+                }
+            });
+
+            instance.table.head.appendChild(that.row);
+
+            instance.on("reset", function(columns) {
+                utils.each(that.inputs, function(input) {
+                    input.value = null;
+                });
+            });
+
+            instance.on("columns.hide", function(columns) {
+                utils.each(columns, function(column) {
+                    that.row.cells[column].style.display = "none";
+                });
+            });
+
+            instance.on("columns.show", function(columns) {
+                utils.each(columns, function(column) {
+                    that.row.cells[column].style.display = "";
+                });
+            });
+
+            instance.on("columns.add", function() {
+                that.add(instance.columns().count());
+            });
+
+            instance.on("columns.remove", function(column) {
+                that.row.removeChild(that.row.cells[column]);
+
+                that.inputs.splice(column, 1);
+            });
+
+            this.initialised = true;
+        };
+
+        Filter.prototype.add = function(cell, placeholder) {
+
+            var index = cell.index || cell;
+
+            var that = this,
+                o = that.config;
+
+            var td = utils.createElement("td", {
+                class: o.classes.filterable
+            });
+            var input = utils.createElement("input", {
+                type: "text",
+                class: o.classes.filter,
+                placeholder: placeholder || ""
+            });
+
+            if (instance.config.fixedColumns) {
+                td.style.width = ((instance.columnWidths[index] / instance.rect.width) * 100) + "%";
+            }
+
+            td.appendChild(input)
+            that.row.appendChild(td);
+
+            that.inputs.push(input);
+        };
+
+        Filter.prototype.destroy = function() {
+            instance.table.head.removeChild(this.row);
+
+            this.initialised = false;
+        };
+
+        return new Filter(options);
+    });
+}
